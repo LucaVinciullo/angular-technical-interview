dd07d9994aa43391530ce5bf7dbe4889e307953f
luca.vinciullo <luca.vinciullo@lynxspa.com>
2022-02-12 11:48:15 +0100

    [Components] Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ComponentsModule} from './components/components.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ComponentsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  imports: [FormsModule],
+  declarations: [],
+  bootstrap: [],
+})
+export class ComponentsModule {}
