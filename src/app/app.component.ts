fd2baadb4a668889e4580c9eecb703e7d18aa90d
LucaVinciullo <vnclcu@hotmail.it>
2022-02-12 11:41:32 +0100

    [TASK] definiti task 1 e task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  date = [
+    '01/01/2022',
+    '01/02/2022',
+    '15/01/2022',
+    '12/02/2018',
+    '02/02/2022',
+    '03/02/2021',
+  ];
+}
