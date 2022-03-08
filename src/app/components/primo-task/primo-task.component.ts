d46ba51d27e437637d8b3e331d1d660f120c9447
luca.vinciullo <luca.vinciullo@lynxspa.com>
2022-03-08 18:30:32 +0100

    [TASK 1]

--- src/app/components/primo-task/primo-task.component.ts
+++ src/app/components/primo-task/primo-task.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './primo-task.component.html'
 })
 export class PrimoTaskComponent {
+task = `Implementare un'esempio del gioco Fizz Buzz
 
+Mostrare nel template html i numeri da 1 a 100. Se il numero è divisibile per 3 sostituirlo con la parola "Fizz". Se il numero è divisibile per 5 sostituirlo con la parola "Buzz". I numeri divisibili sia per 3 che per 5 devono essere sostituiti dalle parole "Fizz Buzz".`
 }
